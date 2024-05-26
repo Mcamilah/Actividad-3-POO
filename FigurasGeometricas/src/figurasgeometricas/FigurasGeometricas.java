acf904997d2e38510aacf3b04a8f73bc891051ff
Mcamilah <161173664+Mcamilah@users.noreply.github.com>
2024-05-26 16:33:01 -0500

    Add files via upload

---
+++ FigurasGeometricas/src/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,11 @@
+
+package figurasgeometricas;
+import gui.Interfaz;
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        Interfaz inter=new Interfaz();
+        inter.setVisible(true);
+        inter.setLocationRelativeTo(null);        
+    }    
+}
